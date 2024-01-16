f238e783eae81463f8ff7f0949e8add302b4cc22
Antonio Jiménez <marshday5@gmail.com>
2024-01-15 22:31:57 -0600

    each component to every route on my spa

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import {Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import { Navbar } from "./components/Navbar";
-
+import { About, Home, Proyects } from "./pages"
 const App = () => {
   return (
     <main className="bg-slate-300/20">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={"Home"}/>
-          <Route path="/about" element={"About"}/>
-          <Route path="/proyects" element={"Proyects"}/>
-          <Route path="/contact" element={"Contact"}/>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/proyects" element={<Proyects/>}/>
+          <Route path="/contact" element={<About/>}/>
         </Routes>
       </Router>
     </main>
---
+++ src/pages/index.jsx
@@ -0,0 +1,6 @@
+import {Home} from "./Home";
+import {Proyects} from "./Proyects";
+import {About} from "./About";
+import {Contact} from "./Contact";
+
+export {Home, Proyects, About, Contact}
