27b85a527e00ddc53d84f9b38232c5ae1a8466ca
Antonio Jiménez <marshday5@gmail.com>
2024-01-16 23:32:30 -0600

    implementing the 3d model on the main screen and working on the lights to my 3d scene

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,21 +4,59 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Loader } from "../components/Loader";
+import { Island } from "../models/Island";
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if(window.innerWidth < 768)
+      {
+        screenScale = [0.9, 0.9, 0.9]; 
+        screenPosition = [0, -6.5, -43]; 
+      }
+    else
+      {
+        screenScale = [1, 1, 1]; 
+      }
+
+    return [screenScale, screenPosition, rotation];
+
+  }
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
-    <section className='w-fill h-screen relative'> 
+    <section className='w-full h-screen relative'> 
       <Canvas 
         className="w-fill h-screen bg-transparent"
         camera={{near:0.1, far: 1000}}
 
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight />        
-          <ambientLight />    
-          <pointLight />    
-          <spotLight />    
-          <hemisphereLight />      
+          <directionalLight 
+            position={[1, 1 ,1]}
+            intensity={2}
+          /> 
+          {/*  <directionalLight /> simula la luz como la del sol que viene de un lado o otro*/}
+
+          <ambientLight 
+            intensity={0.5}
+          />
+          {/* ambientLight ilumina todos los objetos en la escena equitativamente sin castear las sombras     */}    
+          <hemisphereLight 
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+
+          />
+
+          <Island 
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}   
+         />
         </Suspense>
       </Canvas>
     </section>
