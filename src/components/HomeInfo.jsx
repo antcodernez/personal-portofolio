749d026ca5492c15a44e3134a29fd0db618de28b
Antonio Jiménez <marshday5@gmail.com>
2024-01-23 22:58:23 -0600

    popups on the main view

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,27 +1,49 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {arrow} from "../assets/icons"
+const InfoBox = ({text, link, btnText}) => (
+    <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>{text}</p>
+        <Link to={link} className='neo-brutalism-white neo-btn'>
+            {btnText}
+            <img src={arrow} className='w-4 h-4 object-contain'/>
+        </Link>
+    </div>
+)
 const renderContent = {
     1: (
         <h1 className='sm:text-xl sm:leading-snug text-center bg-cyan-600 py-4 px-8 text-white mx-5 rounded-lg'>
-            when the 0's and the <br></br> suenan
+            Hi, I'm <span className='font-bold'>Jesús Antonio</span>, a Mexico-based Software Developer ✌️🚀
         </h1>
     ),
     2: (
-        <h1>2</h1>
+        <InfoBox
+            text="Explore my journey. Let's connect and create something amazing together! 🌟"
+            link="/about"
+            btnText="Connect with me"        
+        />
     ),
     3: (
-        <h1>3</h1>
+        <InfoBox
+            text="Discover exciting projects. Get inspired and let's build something incredible! 🚀"
+            link="/projects"
+            btnText="Explore projects"        
+        />
     ),
     4: (
-        <h1>4</h1>
-    ),
+        <InfoBox
+            text="Explore my insights. Stay informed about the ever-evolving tech landscape! 📰"
+            link="/blog"
+            btnText="Explore blog"        
+        />
+    )
+};
 
-}
-const InfoBox = ({text, link, btnText}) => {
-    <div className='info-box'>
-        {text}
-    </div>
-}
+
+
+    // Los valores del objeto están entre paréntesis porque esos valores son JSX
+    // En JSX, se utilizan paréntesis para envolver expresiones y fragmentos de código JavaScript dentro del código JSX.
+
 const HomeInfo = ({currentStage}) => {
     return renderContent[currentStage] || null;
 }
