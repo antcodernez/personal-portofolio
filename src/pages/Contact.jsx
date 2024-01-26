790f3204dc6b2a04de430f181987744de6cfc8e6
Antonio Jiménez <marshday5@gmail.com>
2024-01-25 22:19:13 -0600

    show alerts

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,8 @@ import emailjs from "@emailjs/browser";
 import { Canvas } from '@react-three/fiber';
 import { Fox } from '../models/Fox';
 import { Loader } from '../components/Loader';
+import {useAlert} from "../hooks/useAlert";
+import { Alert } from '../components/Alert';
 
 const Contact = () => {
 
@@ -12,6 +14,7 @@ const Contact = () => {
   const formRef = useRef(null);
   
   const [isLoading, setIsLoading] = useState(false);
+  const {alert, showAlert, hideAlert} = useAlert();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name] : e.target.value })
@@ -41,22 +44,27 @@ const Contact = () => {
       ).then(() => {
         setIsLoading(false);
         
+        showAlert({show: true, text: "Message sent successfully 😎", type: "success"});
+
         setTimeout(() => {
           setCurrentAnimation("idle");
           setForm({name: "", email: "", message: ""});
-
+          hideAlert({show: false})
         }, 1500);
 
       }).catch(err => {
         setIsLoading(false);
         setCurrentAnimation("idle");
+        showAlert({show: true, text: "I did recive your message", type: "danger"});
         console.log(err);
+
       })
 };
 
 
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
+      {alert.show && <Alert {...alert} />}
       <div className='flex-1 min-w[50%] flex flex-col'>
         <h1 className='head-text'>Get in touch</h1>
 
