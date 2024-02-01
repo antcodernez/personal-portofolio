a722942f100372785cd1a05c03226e84d62510e4
Antonio Jiménez <marshday5@gmail.com>
2024-01-31 21:32:23 -0600

    firsts steps on page proyects

--- src/pages/Proyects.jsx
+++ src/pages/Proyects.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Proyects = () => {
   return (
-    <div>
-      Proyects
-    </div>
+    <section className='max-container'>
+      <h1 className='head-text'>
+          titulo de mientras amor 
+          <span className='blue-gradient_text font-semibold'> Jesus A es la verga 🔥</span>
+      </h1>
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+          My journey in web development began with a passion for creativity and technology. Over time, I honed my skills in creating engaging and functional web experiencess that stand out in an ever-evolving digital world.
+        </p>
+      </div>
+    </section>
   )
 }
 
